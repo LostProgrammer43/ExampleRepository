acad22e2fc0ece701dd5da4403a20c1df7e9e806
Suha <mustafasuha@outlook.com>
2021-01-03 22:47:08 +0300

    Değişiklikler

--- Top-NavigationBar/main.js
+++ Top-NavigationBar/main.js
@@ -1,10 +1,26 @@
-$("button").on("click", function() {
-    var el = $(this);
-    if (el.text() == el.data("text-swap")) {
-      el.text(el.data("text-original"));
-    } else {
-      el.data("text-original", el.text());
-      el.text(el.data("text-swap"));
-    }
-  });
-  
+  var t1 = new TimelineMax({paused: true});
+
+    t1.to(".menu", 1, {
+        left: "0%",
+        ease: Expo.easeInOut,
+        delay: -0.5
+    });
+
+    t1.staggerFrom(".menu ul li", 1, {x: -200, opacity: 0, ease:Expo.easeOut}, 0.2);
+
+    t1.reverse();
+    $(document).on("click", ".header-nav-link", function() {
+     t1.reversed(!t1.reversed());
+    }); 
+
+    $(".menu-prop").on("click", function() {
+        var el = $(this);
+        if (el.text() == el.data("text-swap")) {
+          el.text(el.data("text-original"));
+        } else {
+          el.data("text-original", el.text());
+          el.text(el.data("text-swap"));
+        }
+        
+      });
+      
